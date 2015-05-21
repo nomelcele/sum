a88b69620149985e15244a25c1b9a831b1a480a4
joosang0904 <joosang0904@f915b7c4-6516-0510-836a-91a682e191d0>
2015-05-21 02:27:45 +0000

    김주상
    memberDao에 있던 login메서드 LoginDao로 옮김

--- sumware/src/dao/MemberDao.java
+++ sumware/src/dao/MemberDao.java
@@ -17,48 +17,7 @@ public class MemberDao {
 		return dao;
 	}
 	
-	public MemberVO login(MemberVO v) throws SQLException{
-		   MemberVO vo = new MemberVO();
-		   StringBuffer sql = new StringBuffer();
-		   Connection con = null;
-		   PreparedStatement pstmt = null;
-		   if(v.getMemnum() == 10000){
-			   System.out.println("상급자 없음");
-			   sql.append("select s.memnum, s.memprofile, s.memname, s.memjob , d.dename, nvl(s.memmgr,0) mgrname, s.memauth,s.meminmail ");
-			   sql.append("from member s, dept d where s.memdept=d.denum and s.memnum = ? and s.mempwd =?");
-			   con = ConUtil.getOds();
-			
-			   pstmt = con.prepareStatement(sql.toString());
-		   }else{
-			   System.out.println("상급자 있음");
-			   sql.append("select s.memnum, s.memprofile, s.memname, s.memjob, d.dename, m.memname mgrname, s.memauth,s.meminmail ");
-			   sql.append("from member s, member m, dept d where s.memmgr = m.memnum and s.memdept = d.denum and s.memnum = ? and s.mempwd =?");
-			   con = ConUtil.getOds();
-			
-			   pstmt = con.prepareStatement(sql.toString());
-		   }
-
-
-		   pstmt.setInt(1, v.getMemnum());
-		   pstmt.setString(2, v.getMempwd());
-		   ResultSet rs = pstmt.executeQuery();
-		   if(rs.next()){
-			   vo.setMemnum(rs.getInt("memnum"));
-			   vo.setMemprofile(rs.getString("memprofile"));
-			   vo.setMemname(rs.getString("memname"));
-			   vo.setMemjob(rs.getString("memjob"));
-			   vo.setDename(rs.getString("dename"));
-			   vo.setMgrname(rs.getString("mgrname"));
-			   vo.setMemauth(rs.getInt("memauth"));
-			   vo.setMeminmail(rs.getString("meminmail"));
-		   }else{
-			   return null;
-		   }
-		   CloseUtil.close(rs);
-		   CloseUtil.close(pstmt);
-		   CloseUtil.close(con);
-		  return vo;
-	}
+	
 	
 	
 }
