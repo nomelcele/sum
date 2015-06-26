4813c15fa44d5cf2b6095920e5d74502cbedc91c
joosang0904 <joosang0904@f915b7c4-6516-0510-836a-91a682e191d0>
2015-06-26 08:58:31 +0000

    완성!

--- sumware/src/com/sumware/mvc/model/TodoModel.java
+++ sumware/src/com/sumware/mvc/model/TodoModel.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.sumware.dto.CalendarVO;
 import com.sumware.dto.MemberVO;
+import com.sumware.dto.TodoJobVO;
 import com.sumware.dto.TodoVO;
 import com.sumware.mvc.dao.CalendarDAO;
 import com.sumware.mvc.dao.TodoDao;
@@ -102,6 +103,7 @@ public class TodoModel{
 	// 팀장의 업무 승인!!!! y로 바꿔줌!! 캘린더에도 등록!!!!
 	@RequestMapping(value="/approveTodo", method=RequestMethod.POST)
 	public String approveTodo(TodoVO tvo){
+		// 리스트의 승인여부 n을 y로 바꿈!!!!
 		tdao.confirmTodo(tvo, "y");
 //		map.put("start",map.get("tostdate"));
 //		map.put("end", map.get("toendate"));
@@ -117,134 +119,82 @@ public class TodoModel{
 		return "todo/checkTodoList";
 	}
 	
-
+	// 팀장의 업무 거절!!!
+	@RequestMapping(value="/rejectTodo", method=RequestMethod.POST)
+	public String rejectTodo(TodoVO tvo){
+		// 리스트의 승인여부 n을 z로 바꿈!!!!
+		
+		tdao.confirmTodo(tvo, "z");
+		return "todo/checkTodoList";
+	}
 	
-//		}else if(submod.equals("checkTodoList")){
-//			// 업무 관리 클릭시 보여줄 화면 , 업무 리스트
-//
-//			String childmod = request.getParameter("childmod");
-//			if(childmod!=null && childmod.equals("approveTodo")){
-//				System.out.println("approveTodo 들어옴!!");
-//				// 리스트의 승인여부 n을 y로 바꿈!!!!
-//				HashMap<String,String> map = MyMap.getMaps().getMapList(request);
-////				TodoDao.getDao().confirmTodo(map, "y");
-//				//캘린더에 등록
-//				map.put("start",map.get("tostdate"));
-//				map.put("end", map.get("toendate"));
-//				map.put("title", map.get("totitle"));
-//				map.put("cal", map.get("todept"));
-//				String sql = CalendarDAO.getDao().calSQL(2);
-//				CalendarDAO.getDao().calInsert(sql, map);
-//				
-//			}else if(childmod!=null && childmod.equals("rejectTodo")){
-//				// 리스트의 승인여부 n을 z로 바꿈!!!!
-//				System.out.println("rejectTodo 들어옴!!");
-//				HashMap<String,String> map = MyMap.getMaps().getMapList(request);
-//				//TodoDao.getDao().confirmTodo(map, "z");
-//			}
-//			
-//			url = "sumware?model=todo&submod=checkTodoListForm";
-//			method = true;
-//			
-//		}else if(submod.equals("checkTodoListForm")){
-//			int tomem = Integer.parseInt(request.getParameter("memnum"));
-//			//ArrayList<TodoVO> clist = TodoDao.getDao().checkTodoList(tomem);
-//			//request.setAttribute("todoList", clist);
-//			
-//			url = "todo/checkTodoList.jsp";
-//			method = true;
-//		}else if(submod.equals("fWMana")){
-//			System.out.println("fWMana 들어옴");
-//			int memnum = Integer.parseInt(request.getParameter("memnum"));
-//			System.out.println("memnum:"+memnum);
-//
-//			//ArrayList<TodoVO> fwList=TodoDao.getDao().getFWMana(memnum);
-////			request.setAttribute("fwList", fwList);
-//			url="todo/fWMana.jsp";
-//			method=true;
-//		}else if(submod.equals("toUpFk")){
-//			System.out.print("toUpFk 들어옴");
-//			
-//			HashMap<String, String> map = MyMap.getMaps().getMapList(request);
-//			//ArrayList<TodoVO> list = TodoDao.getDao().todoUpdate(map);
-//			System.out.println("dao로 todoupdate");
-//			
-//			//request.removeAttribute("fwList");
-////			request.setAttribute("fwList", list);
-//			
-//			url="todo/fWMana.jsp";
-//			method=true;
-//		}else if(submod.equals("giveJobForm")){
-//			System.out.println("giveJobForm들어옴");
-//			HashMap<String, String> map = MyMap.getMaps().getMapList(request);
-//			System.out.println("request받아옴");
-//			//ArrayList<TodoVO> todoList = TodoDao.getDao().getTeamTodoList(map);
-//			System.out.println("todolist받아옴");
-//			int memmgr = Integer.parseInt(map.get("memnum"));
-//			//ArrayList<MemberVO> teamMemberList = TodoDao.getDao().getTomem(memmgr);
-//			System.out.println("teammembers받아옴");
-////			request.setAttribute("teamTodoList", todoList);
-////			request.setAttribute("teamMemberList", teamMemberList);
-//
-//			url = "todo/giveJob.jsp";
-//			method = true;
-//			
-//		}else if(submod.equals("insertMemJob")){
-//			
-//			System.out.print("insertMemJob 들어옴");
-//			HashMap<String, String> map = MyMap.getMaps().getMapList(request);
-//			//TodoDao.getDao().insertMemJob(map);
-//			
-//			url = "sumware?model=todo&submod=showMembersJob";
-//			method = true;
-//			
-//		}else if(submod.equals("showMembersJob")){
-//			System.out.println("showMembersJob들어옴");
-//			int jobtonum = Integer.parseInt(request.getParameter("jobtonum"));
-//			System.out.println("jobtonum : "+jobtonum);
-//			//ArrayList<TodoJobVO> membersjoblist = TodoDao.getDao().getMembersJob(jobtonum);
-//			// todoJobVO확인
-//			//for(TodoJobVO v : membersjoblist){
-////				System.out.println("memname : " + v.getMemname());
-////				System.out.println("memprofile : " + v.getMemprofile());
-////				System.out.println("jobcont : " + v.getJobcont());
-////			}
-//
-//			//request.setAttribute("membersjoblist", membersjoblist);
-//			url = "todo/membersJob.jsp";
-//			method = true;
-//			
-//		}else if(submod.equals("showmemlist")){
-//			// 부서업무부분, 팀업무부분에서 자세히보기 하면 정보들이 나옴
-//			System.out.println("showmemlist들어옴");
-//			int jobtonum = Integer.parseInt(request.getParameter("jobtonum"));
-//			System.out.println("jobtonum : "+jobtonum);
-//			//ArrayList<TodoJobVO> list = TodoDao.getDao().getMembersJob(jobtonum);
-//			//request.setAttribute("memberjoblist", list);
-//			url = "todo/jobDetail.jsp";
-//			method = true;
-//			
-//		}else if(submod.equals("teamTodoForm")){
-//			// 팀별 업무 리스트 뽑아오기
-//			System.out.println("teamTodoForm들어옴");
-//			int memmgr = Integer.parseInt(request.getParameter("memmgr"));
-//			System.out.println("memmgr : "+ memmgr);
-//			//ArrayList<TodoVO> list = TodoDao.getDao().getTeamJob(memmgr);
-//			//request.setAttribute("teamJobList", list);
-//			
-//			url = "todo/teamTodoForm.jsp";
-//			method = true;
-//			
-//		}else if(submod.equals("successJob")){
-//			// 팀장의 업무 성공 처리
-//			HashMap<String,String> map = MyMap.getMaps().getMapList(request);
-//			//TodoDao.getDao().confirmTodo(map, "o");
-//			
-//			url = "sumware?model=todo&submod=teamTodoForm";
-//			method = true;
-//			
-//		}
-//		
-//		return new ModelForward(url, method);
-//	}
+	@RequestMapping(value= "/fWMana", method=RequestMethod.POST)
+	public String fWMana(MemberVO mvo, Model model){
+		List<TodoVO> fwList = tdao.getFWMana(mvo.getMemnum());
+		model.addAttribute("fwList", fwList);
+		
+		return "todo/fWMana";
+	}
+	
+	@RequestMapping(value="/toUpFk", method=RequestMethod.POST)
+	public String toUpFk(TodoVO tvo, Model model){
+		List<TodoVO> list = tdao.todoUpdate(tvo);
+//		//request.removeAttribute("fwList");
+		model.addAttribute("fwList", list);
+		return "todo/fWMana";
+	}
+	
+	// 업무 부여 폼
+	@RequestMapping(value="/giveJobForm",  method=RequestMethod.POST)
+	public String giveJobForm(MemberVO mvo, Model model){
+		List<TodoVO> todoList = tdao.getTeamTodoList(mvo);
+		int memmgr = mvo.getMemnum();
+		List<MemberVO> teamMemberList = tdao.getTomem(memmgr);
+		
+		model.addAttribute("teamTodoList", todoList);
+		model.addAttribute("teamMemberList", teamMemberList);
+		
+		return "todo/giveJob";
+		
+	}
+
+	// 업무 부여
+	@RequestMapping(value="/insertMemJob", method=RequestMethod.POST )
+	public String insertMemJob(TodoJobVO tjvo){
+		tdao.insertMemJob(tjvo);
+		return "redirect:/showMembersJob";
+	}
+	
+	// 업무 보여줌?
+	@RequestMapping(value="/showMembersJob", method=RequestMethod.POST)
+	public String showMembersJob(TodoJobVO tjvo,Model model){
+		List<TodoJobVO> membersjoblist = tdao.getMembersJob(tjvo.getJobtonum());
+		model.addAttribute("membersjoblist", membersjoblist);
+		return "todo/membersJob";
+	}
+
+	// 멤버리스트?
+	@RequestMapping(value="/showmemlist",  method=RequestMethod.POST)
+	public String showmemlist(TodoJobVO tjvo, Model model){
+		List<TodoJobVO> list = tdao.getMembersJob(tjvo.getJobtonum());
+		model.addAttribute("memberjoblist", list);
+		return "todo/jobDetail";
+	}
+
+	// 팀 업무 폼?
+	@RequestMapping(value="/teamTodoForm",  method=RequestMethod.POST)
+	public String teamTodoForm(MemberVO mvo, Model model){
+		List<TodoVO> list = tdao.getTeamJob(mvo.getMemmgr());
+		model.addAttribute("teamJobList", list);
+		return "todo/teamTodoForm";
+	}
+	
+	//업무 완료 처리 
+	@RequestMapping(value="/successJob",  method=RequestMethod.POST)
+	public String successJob(Model model, TodoVO tvo ){
+		tdao.confirmTodo(tvo, "o");
+		return "redirect:/teamTodoForm";
+	}
+
+
 }
