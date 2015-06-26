0568c5abda79049b5cc11ed4d183f6d3eb235c30
audvy67 <audvy67@f915b7c4-6516-0510-836a-91a682e191d0>
2015-06-26 12:04:20 +0000

    스프링 1차진행중

--- sumware/src/com/sumware/mvc/dao/LoginDao.java
+++ sumware/src/com/sumware/mvc/dao/LoginDao.java
@@ -5,62 +5,22 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.sumware.dto.MemberVO;
 import com.sumware.util.CloseUtil;
 
 import conn.ConUtil;
-
+@Repository
 public class LoginDao {
-	private static LoginDao dao;
-
-	public synchronized static LoginDao getDao() {
-		if (dao == null)
-			dao = new LoginDao();
-		return dao;
-	}
+	@Autowired
+	private SqlSessionTemplate st;
 
 	// 로그인 시 사용하는 메서드
-	public MemberVO login(MemberVO v) throws SQLException{
-		   MemberVO vo = new MemberVO();
-		   StringBuffer sql = new StringBuffer();
-		   Connection con = null;
-		   PreparedStatement pstmt = null;
-
-			   System.out.println("상급자 있음");
-			   sql.append("select s.memnum, s.mempwd, s.memprofile, s.memname, s.memjob,s.memdept,s.memmgr, d.dename, m.memname mgrname, s.memauth,s.meminmail ");
-			   sql.append("from member s, member m, dept d where s.memmgr = m.memnum and s.memdept = d.denum and s.memnum = ? and s.mempwd =?");
-			   // 부서 번호와 상급자 번호가 있어야 한다.
-			   // 부서 번호: 900, 상급자 번호: 1 (default)
-			   con = ConUtil.getOds();
-			
-			   pstmt = con.prepareStatement(sql.toString());
-	
-
-		   pstmt.setInt(1, v.getMemnum());
-		   System.out.println("로그인한 사원 번호: "+v.getMemnum());
-		   pstmt.setString(2, v.getMempwd());
-		   System.out.println("로그인한 사원 비번: "+v.getMempwd());
-		   ResultSet rs = pstmt.executeQuery();
-		   
-		   if(rs.next()){
-			   vo.setMemnum(rs.getInt("memnum"));
-			   vo.setMemprofile(rs.getString("memprofile"));
-			   vo.setMemname(rs.getString("memname"));
-			   vo.setMemjob(rs.getString("memjob"));
-			   vo.setDename(rs.getString("dename"));
-			   vo.setMgrname(rs.getString("mgrname"));
-			   vo.setMemauth(rs.getInt("memauth"));
-			   vo.setMeminmail(rs.getString("meminmail"));
-			   vo.setMemdept(rs.getInt("memdept"));
-			   vo.setMemmgr(rs.getInt("memmgr"));
-			   vo.setMempwd(rs.getString("mempwd"));
-		   }else{
-			   return null;
-		   }
-		   CloseUtil.close(rs);
-		   CloseUtil.close(pstmt);
-		   CloseUtil.close(con);
-		  return vo;
+	public MemberVO login(MemberVO mvo) throws SQLException{
+		return st.selectOne("login.login", mvo);
 	}
 	
 	public String ckFirstLogin(int memnum,String mempwd){
