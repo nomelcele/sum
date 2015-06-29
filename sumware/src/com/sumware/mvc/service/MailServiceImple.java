0f00d362c2304cf11b271da627ea5b44e0be3e0f
harzen_09 <harzen_09@f915b7c4-6516-0510-836a-91a682e191d0>
2015-06-29 08:52:32 +0000

    service qualifier

--- sumware/src/com/sumware/mvc/service/MailServiceImple.java
+++ sumware/src/com/sumware/mvc/service/MailServiceImple.java
@@ -3,6 +3,7 @@ package com.sumware.mvc.service;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +12,7 @@ import com.sumware.mvc.dao.MailDao;
 
 @Transactional
 @Service
+@Qualifier(value="mail")
 public class MailServiceImple extends AbstractService{
 	@Autowired
 	private MailDao mdao;
@@ -20,15 +22,17 @@ public class MailServiceImple extends AbstractService{
 		for(String e:mailnums){
 			MailVO vo = mdao.getDelAttrMailInfo(e);
 			
-			int mailmem = vo.getMailmem();
-			String mailreceiver = vo.getMailreceiver();
+			int mailmem = vo.getMailmem(); // 삭제할 메일의 발신자
+			String mailreceiver = vo.getMailreceiver(); // 삭제할 메일의 수신자
 			
 			if(mailmem == Integer.parseInt(map.get("usernum"))){
-				map.put("mailnum", e);
+				// 삭제할 메일이 로그인한 사용자가 보낸 메일인 경우
+				map.put("mailnum", e); // 메일 번호
 				mdao.setDelAttrFrom(map);
 			}
 			
 			if(mailreceiver.equals(map.get("userid"))){
+				// 삭제할 메일이 로그인한 사용자가 받은 메일인 경우
 				map.put("mailnum", e);
 				mdao.setDelAttrTo(map);
 				
--- sumware/src/com/sumware/mvc/service/MessengerServiceImple.java
+++ sumware/src/com/sumware/mvc/service/MessengerServiceImple.java
@@ -3,6 +3,7 @@ package com.sumware.mvc.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import com.sumware.mvc.dao.MessengerDao;
 import conn.ConUtil;
 @Transactional
 @Service
+@Qualifier(value="messenger")
 public class MessengerServiceImple extends AbstractService{
 	@Autowired
 	private MessengerDao mdao;
